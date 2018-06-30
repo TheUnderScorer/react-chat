ada123dd17f1e97d6780a6097bf9e14992d1d98e
Przemysław Żydek <przemek543@gmail.com>
2018-06-30 13:39:16 +0200

    Support for setting default value for input

--- client/src/components/form/Input.js
+++ client/src/components/form/Input.js
@@ -3,34 +3,76 @@ import PropTypes from 'prop-types';
 
 class Input extends Component {
 
+	constructor() {
+		super();
+
+		this.state = {
+			value: '',
+		}
+
+	}
+
 	static defaultProps = {
-		name:    '',
-		id:      '',
-		type:    'text',
-		value:   '',
-		label:   '',
-		checked: false
+		name:        '',
+		id:          '',
+		type:        'text',
+		value:       '',
+		label:       '',
+		checked:     false,
+		disabled:    false,
+		placeholder: '',
 	};
 
 	static propTypes = {
-		name:    PropTypes.string,
-		id:      PropTypes.string,
-		type:    PropTypes.string,
-		value:   PropTypes.any,
-		label:   PropTypes.string,
-		checked: PropTypes.bool
+		name:        PropTypes.string,
+		id:          PropTypes.string,
+		type:        PropTypes.string,
+		value:       PropTypes.any,
+		label:       PropTypes.string,
+		checked:     PropTypes.bool,
+		disabled:    PropTypes.bool,
+		placeholder: PropTypes.string
 	};
+
+	handleChange( e ) {
+		this.setState( {
+			value: e.target.value,
+		} )
+	}
 
 	getInput() {
 
-		let props = this.props;
+		let props = this.props,
+			state = this.state;
 
 		if ( props.type === 'checkbox' || props.type === 'radio' ) {
 			return (
-				<input key="1" className={'md-' + props.type} type={props.type} name={props.name} id={props.id} defaultValue={props.value} defaultChecked={props.checked ? 'checked' : ''}/>);
+				<input
+					key="1"
+					placeholder={props.placeholder}
+					className={'md-' + props.type}
+					type={props.type}
+					name={props.name}
+					id={props.id}
+					value={props.value}
+					defaultValue={props.value}
+					defaultChecked={props.checked ? 'checked' : ''}
+				/>);
 		} else {
+
+			let value = state.value === '' ? props.value : state.value;
+
 			return (
-				<input key="1" type={props.type} name={props.name} id={props.id} defaultValue={props.value}/>);
+				<input
+					key="1"
+					type={props.type}
+					placeholder={props.placeholder}
+					name={props.name}
+					id={props.id}
+					value={value}
+					disabled={props.disabled}
+					onChange={this.handleChange.bind( this )}
+				/>);
 		}
 
 	}
